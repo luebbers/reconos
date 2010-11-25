161fcdba6c714cf8aec2f55cd6e32d491c85d13e
Enno Luebbers <luebbers@reconos.de>
2010-11-24 23:55:20 -0800

    Added remote programming to download_bitstream.sh

--- tools/impact/download_bitstream.sh
+++ tools/impact/download_bitstream.sh
@@ -5,6 +5,8 @@
 # Usage: download_bitstream.sh <bitfile> [jtag chain pos]
 #        - jtag chain pos defaults to 3
 #
+# If the environment variable IMPACT_REMOTE is set, it is used
+# as a remotely running cse_server instead of a local USB connection
 
 POS=3
 if [ $# -eq 2 ]
@@ -12,6 +14,7 @@ then
 	POS=$2
 fi
 
+if [ -z $IMPACT_REMOTE ]; then
 
 echo "
 setMode -bs
@@ -21,11 +24,35 @@ IdentifyMPM
 assignFile -p $POS -file \"$1\"
 Program -p $POS
 quit
-" | impact -batch && echo "
+" | impact -batch
+
+else
+echo "Using remote cs_server at ${IMPACT_REMOTE}"
+echo "
+setMode -bs
+setCable -port auto -server ${IMPACT_REMOTE}
+Identify
+IdentifyMPM
+assignFile -p $POS -file \"$1\"
+Program -p $POS
+quit
+" | impact -batch
+
+fi
+
+# test for success
+if [ $? -eq 0 ]; then
+   echo "
 
 SUCCESS
-bitstream is $(($(date "+%s") - $(stat -c "%Y" $1))) seconds old" || echo "
+bitstream is $(($(date "+%s") - $(stat -c "%Y" $1))) seconds old" 
+
+else
+    echo "
 
 FAILURE
 could not download bitstream"
+    exit 1
+fi
+
 
