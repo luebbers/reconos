afca9d5286f98f2ebc41a9a261ac27bcbb335da3
Enno Luebbers <luebbers@reconos.de>
2011-02-14 16:07:36 +0100

    Add -l and -a options to reconos_addhwthread.py
    -l tells reconos_addhwthread to link the thread sources instead of copying
    them.
    -a allows specification of the target FPGA architecture for compiling the
    thread, e.g.
    
        reconos_addhwthread.py -a "virtex2p" thread_name thread_entity thread.vhd

--- tools/python/reconos_addhwthread.py
+++ tools/python/reconos_addhwthread.py
@@ -36,7 +36,9 @@ Creates a new hardware thread inside a ReconOS project
 import reconos.tools, sys, os, string, shutil, getopt
 
 def usage():
-    sys.stderr.write('USAGE: ' + os.path.basename(sys.argv[0]) + '<hwthread_name> <user_logic_entity> [<first_file> <second_file> ...]\n')
+    sys.stderr.write('USAGE: ' + os.path.basename(sys.argv[0]) + ' [-g "GENERIC=>1"] [-l] <hwthread_name> <user_logic_entity> [<first_file> <second_file> ...]\n')
+    sys.stderr.write('   -g "GENERIC => 1"   set generic of hw thread\n')
+    sys.stderr.write('   -l                  link files instead of copying\n')
     sys.stderr.write('If you don\'t specify any files, you\'ll have to manually copy them\ninto the created directory and add them to the top of the Makefile.\n')
     sys.stderr.write('Actually, that is what you have to do if you want to add netlists (.ngc/.edn) to your hardware thread.\n')
 	
@@ -44,13 +46,15 @@ def usage():
 def main(arguments):
 	
     try:
-        opts, args = getopt.getopt(arguments, "g:", ["generic="])
+        opts, args = getopt.getopt(arguments, "lg:a:", ["link", "generic=", "architecture="])
     except getopt.GetoptError, err:
         print str(err)
         usage()
         sys.exit(2)
 
     generics = []
+    link = False
+    arch = "virtex6"
 
     for o, a in opts:
         if o in ("-g", "--generic"):
@@ -58,6 +62,10 @@ def main(arguments):
             # addthread -g "C_THREAD_NUM : integer := 2" ...
             # TODO: catch invalid generics?
             generics.append(a)
+        elif o in ("-l", "--link"):
+            link = True
+        elif o in ("-a", "--architecture"):
+            arch = a;
         else:
             assert False, "unhandled option"
 			
@@ -76,15 +84,18 @@ def main(arguments):
     # create hw thread directory
     os.mkdir(hwthread_name)
 
-    # copy thread files
+    # copy or link thread files
     for f in files:
-        shutil.copy(f, hwthread_name)
+        if link:
+            os.symlink(os.path.abspath(f), hwthread_name + "/" + os.path.basename(f))
+        else:
+            shutil.copy(f, hwthread_name)
 
     # set up substitutions for Makefile template
     subst = [ 
         ('\$template:vhdl_files\$', string.join([ os.path.basename(f) for f in files ], ' ')),
         ('\$template:user_logic_entity\$', user_logic_entity),
-        ('\$template:architecture\$', 'virtex2p'),
+        ('\$template:architecture\$', arch),
 		('\$template:generics\$', "\"" + ",".join(generics) + "\"")
     ]
     templ_name = os.environ['RECONOS'] + '/tools/makefiles/templates/Makefile_hw_hwthreads_thread.template'
